20f9d700e9bcac018115a2e4c3d67800cb1d3ef5
Swapnil Tomake <swapnil@inspiremindstech.com>
2021-09-01 21:07:11 +0530

    Revert "changes in igx-grid and working on user-grid-component"
    This reverts commit 1eee2bd4a172428ac5b4f96f82132a0e3f2149f3.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'user-grid';
-  localData=[
-    {
-      "First Name":"Swapnil",
-      "Last Name":"Tomake",
-      "Email":"swapniltomake999@gmail.com"
-    }
-  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { IgxGridModule } from 'igniteui-angular';
-import { UserGridItemComponent } from './components/user-grid-item/user-grid-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserGridItemComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    IgxGridModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
